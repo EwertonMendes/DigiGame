0ba7fc5c30b54ae8f1d231bc71667071e1bd6c0a
Ewerton Mendes <ewerton.mendes@terceirizadoraizen.com>
2023-09-20 21:20:45 -0300

    feat: adds different types of terrain on terrain generation script

--- TileMap.gd
+++ TileMap.gd
@@ -5,20 +5,32 @@ var GridSizeY = 50
 var Dic = {}
 var selectedTile
 
-var tileTypes = {
-	"DIRT": Vector2i(0,0),
-	"GRASS": Vector2i(2,0),
-}
+var tileTypes = [
+	{ 
+		type = "DIRT",
+		position = Vector2i(0,0)
+	},
+	{ 
+		type = "GRASS",
+		position = Vector2i(2,0)
+	},
+	{ 
+		type = "SNOW",
+		position = Vector2i(5,0)
+	},
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in GridSizeX:
 		for y in GridSizeY:
+			var tileType = tileTypes.pick_random()
 			Dic[str(Vector2(x,y))] = {
-				"Type": "GRASS",
-				"Position": str(Vector2(x,y))
+				"type": tileType.type,
+				"position": str(Vector2(x,y))
 			}
-			set_cell(0, Vector2(x,y), 1, tileTypes["GRASS"], 0)
+			#randi() % 3
+			set_cell(0, Vector2(x,y), 1, tileType.position, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,6 +45,10 @@ func _process(delta):
 		selectedTile = map_to_local(tile)
 		
 		var hoveredTile = Dic.get(str(tile))
+		var tileType = findTileByType(hoveredTile["type"])
+		set_cell(1, tile, 0, tileType.position, 0)
 		
-		set_cell(1, tile, 0, tileTypes[hoveredTile["Type"]], 0)
-		#print(selectedTile)
+func findTileByType(type: String):
+	for tileType in tileTypes:
+		if tileType["type"] == type:
+			return tileType
