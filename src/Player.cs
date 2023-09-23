fa0fb55c97d61a76ec34d89dd048fbc04b08e51e
Ewerton Mendes <ewerton.mendes@terceirizadoraizen.com>
2023-09-23 20:47:55 -0300

    feat: moves digimon only when it's selected

--- src/Player.cs
+++ src/Player.cs
@@ -5,7 +5,12 @@ public partial class Player : CharacterBody2D
 	private readonly Vector2 PLAYER_POSITION_DEVIATION = new(0, 4);
 	private Vector2 initialTileCoords = new(48, 70);
 
-    public override void _Ready()
+	private bool isSelected = false;
+
+	private Vector2 selectedTileCoords;
+
+
+	public override void _Ready()
 	{
 		GlobalPosition = initialTileCoords - PLAYER_POSITION_DEVIATION;
 		GetNode<AnimationPlayer>("AnimationPlayer").Play("walk_up");
@@ -13,22 +18,46 @@ public partial class Player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Move(delta);
-		MoveMouse();
-		MoveAndSlide();
+		selectedTileCoords = (Vector2)GetNode("../Blocks").Get("selectedTile");
 	}
 
-	private void Move(double delta)
+	public override void _Input(InputEvent @event)
 	{
-		Velocity = Vector2.Zero;
-	}
+		@event = @event as InputEventMouseButton;
+		if (@event == null) return;
 
-	private void MoveMouse()
-	{
-		if (Input.IsActionJustPressed("LeftClick"))
+		if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.ButtonIndex == MouseButton.Left)
 		{
-			var selectedTilePosition = (Vector2)GetNode("../Blocks").Get("selectedTile");
-			GlobalPosition = selectedTilePosition - PLAYER_POSITION_DEVIATION;
+			if (eventMouseButton.IsReleased() && !IsDigimonPositionClicked())
+			{
+				isSelected = false;
+				return;
+			}
+
+			if (eventMouseButton.IsPressed() && isSelected)
+			{
+				MoveDigimonPosition();
+				isSelected = false;
+				return;
+			}
+
+			if (eventMouseButton.IsPressed() && IsDigimonPositionClicked())
+			{
+				isSelected = true;
+				return;
+			}
 		}
 	}
+
+	private bool IsDigimonPositionClicked()
+	{
+		var selectedTilePosition = (Vector2)GetNode("../Blocks").Get("selectedTile");
+		return selectedTilePosition == (GlobalPosition + PLAYER_POSITION_DEVIATION);
+	}
+
+	private void MoveDigimonPosition()
+	{
+		GlobalPosition = selectedTileCoords - PLAYER_POSITION_DEVIATION;
+	}
+
 }
