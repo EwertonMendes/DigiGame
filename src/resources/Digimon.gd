103a0c0128ca1d5ffaf69c4cfbc279cc4ef26a7d
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-09-09 03:05:34 -0300

    fix: normalize Digimon resource properties for GDScript

--- src/resources/Digimon.gd
+++ src/resources/Digimon.gd
@@ -1,19 +1,19 @@
 extends Resource
 class_name Digimon
 
-@export var Texture: Texture2D
-@export var InitialFrame: int
-@export var SpriteDeviation: Vector2
-@export var ParticleDeviation: Vector2
-@export var InitialPosition: Vector2i
-@export var Type: String
-@export var Name: String
-@export var Level: int
-@export var Hp: int
-@export var Mp: int
-@export var Attack: int
-@export var Defense: int
-@export var Age: int
-@export var Battles: int
-@export var Victories: int
-@export var Defeats: int
+@export var texture: Texture2D
+@export var initial_frame: int
+@export var sprite_deviation: Vector2
+@export var particle_deviation: Vector2
+@export var initial_position: Vector2i
+@export var type: String
+@export var display_name: String
+@export var level: int
+@export var hp: int
+@export var mp: int
+@export var attack: int
+@export var defense: int
+@export var age: int
+@export var battles: int
+@export var victories: int
+@export var defeats: int
