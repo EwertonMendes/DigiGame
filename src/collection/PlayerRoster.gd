23cf2f5fa5b8cb53165128f03f046c3e038e62d7
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-09-13 14:26:32 -0300

    refactor(collection): remove PlayerRoster compatibility shim

--- src/collection/PlayerRoster.gd
+++
@@ -1,5 +0,0 @@
-extends "res://src/collection/PlayerCollection.gd"
-class_name PlayerRoster
-
-# Compatibility shim for code paths being migrated in this PR. New gameplay,
-# save data, UI and services use PlayerCollection.
