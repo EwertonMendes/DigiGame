f49af90c1a68a8fe8d61940b0aedbe94d0e52589
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-09-15 00:21:18 -0300

    ui: make Party Storage expose the Ascension primary tab safely

--- src/ui/DigiLabPartyStorageScreen.gd
+++ src/ui/DigiLabPartyStorageScreen.gd
@@ -9,11 +9,14 @@ func open_screen() -> void:
 	_header.configure_tabs(PrimaryTabs.specs(), "party")
 	_header.set_active_tab("party")
 	_hint_bar.set_primary_tabs_enabled(true)
+	var ascension_handler := Callable(self, "_on_ascension_tab_selected")
+	if not _header.tab_selected.is_connected(ascension_handler):
+		_header.tab_selected.connect(ascension_handler)
 
 
-func _on_top_tab_selected(tab_id: String) -> void:
-	if tab_id != "party":
-		tab_requested.emit(tab_id)
+func _on_ascension_tab_selected(tab_id: String) -> void:
+	if tab_id == "ascension":
+		tab_requested.emit("ascension")
 
 
 func get_selected_instance_id() -> String:
