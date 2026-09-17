98ee381f896bcfc7d27139ebce213610e18aeca2
Ewerton Mendes <ewertonmendes9@gmail.com>
2026-09-16 21:30:47 -0300

    Support precise horizontal analog navigation

--- src/ui/components/DigiAnalogNavigationGate.gd
+++ src/ui/components/DigiAnalogNavigationGate.gd
@@ -3,16 +3,20 @@ class_name DigiAnalogNavigationGate
 
 var vertical_press_threshold := 0.62
 var vertical_release_threshold := 0.34
+var horizontal_press_threshold := 0.62
+var horizontal_release_threshold := 0.34
 var trigger_press_threshold := 0.55
 var trigger_release_threshold := 0.25
 
 var _vertical_state := 0
+var _horizontal_state := 0
 var _left_trigger_down := false
 var _right_trigger_down := false
 
 
 func reset() -> void:
 	_vertical_state = 0
+	_horizontal_state = 0
 	_left_trigger_down = false
 	_right_trigger_down = false
 
@@ -30,6 +34,22 @@ func vertical_step(value: float) -> int:
 	if direction == 0 or direction == _vertical_state:
 		return 0
 	_vertical_state = direction
+	return direction
+
+
+func horizontal_step(value: float) -> int:
+	if absf(value) <= horizontal_release_threshold:
+		_horizontal_state = 0
+		return 0
+
+	var direction := 0
+	if value <= -horizontal_press_threshold:
+		direction = -1
+	elif value >= horizontal_press_threshold:
+		direction = 1
+	if direction == 0 or direction == _horizontal_state:
+		return 0
+	_horizontal_state = direction
 	return direction
 
 
